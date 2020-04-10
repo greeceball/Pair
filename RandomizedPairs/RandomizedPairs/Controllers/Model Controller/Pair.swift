12316255302470b74c367201f4f4bbe67d8b5d95
Colby Harris <griff33@gmail.com>
2020-04-10 11:15:04 -0600

    Added pair model

---
+++ RandomizedPairs/RandomizedPairs/Controllers/Model Controller/Pair.swift
@@ -0,0 +1,19 @@
+//
+//  Pair.swift
+//  RandomizedPairs
+//
+//  Created by Colby Harris on 4/10/20.
+//  Copyright © 2020 Colby_Harris. All rights reserved.
+//
+
+import Foundation
+
+class Pair {
+    let name1: String
+    let name2: String
+    
+    init(name1: String, name2: String) {
+        self.name1 = name1
+        self.name2 = name2
+    }
+}
