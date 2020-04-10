d77b6afba7f9f96c7f83949e14f802d357f58e64
Colby Harris <griff33@gmail.com>
2020-04-10 11:13:01 -0600

    Added Person Model

---
+++ RandomizedPairs/RandomizedPairs/Controllers/Model Controller/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  RandomizedPairs
+//
+//  Created by Colby Harris on 4/10/20.
+//  Copyright © 2020 Colby_Harris. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
